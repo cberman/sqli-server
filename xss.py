0a4e3b62496f754af3f55ce7f455880d5a63abe3
Cyrus Malekpour <cmalekpour@gmail.com>
2015-09-24 16:14:38 -0400

    Changed port to not conflict with SQLi server

--- xss.py
+++ xss.py
@@ -120,4 +120,4 @@ def submit5():
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
